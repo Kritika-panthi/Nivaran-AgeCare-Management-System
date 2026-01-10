39decfc9a6e20496770c61fa7186e9a6272f4497
Kritee <kritikapanthi45@gmail.com>
2026-01-10 10:00:00 +0545

    Initial React + Vite project setup

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+//tools that checks code for errors
 export default defineConfig([
   globalIgnores(['dist']),
   {
