9b4eb01f34c8394464752977c4d2291a9252ffb3
Kritee <kritikapanthi45@gmail.com>
2026-01-10 10:00:00 +0545

    Setup application structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,72 @@
-import './App.css'
 import { Routes, Route } from "react-router-dom";
-import Login from './Pages/LoginPage';
-import ClientRegisterPage from './Pages/ClientRegisterPage';
-import CaregiverRegisterPage from './Pages/CaregiverRegisterPage';
+import Home from "./Pages/Home";
+import About from "./Pages/Aboutus";
+import Services from "./Pages/Service";
+import Contact from "./Pages/ContactUs";
+
+
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
+
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-
   return (
-    <>
+    <AuthProvider>
+      <ScrollToTop />
+
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/clientregister" element={<ClientRegisterPage />} />
-        <Route path="/caregiverregister" element={<CaregiverRegisterPage />} />
+
+        {/* Public Pages With Layout */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route
+          path="/services"
+          element={
+            <>
+              <Header />
+              <Services />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route
+          path="/contact"
+          element={
+            <>
+              <Header />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+
       </Routes>
-    </>
-  )
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
