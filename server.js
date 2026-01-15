e69bc90d09a311c85d73190b1b5d4ec3b6705628
Kritee <kritikapanthi45@gmail.com>
2026-01-15 10:00:00 +0545

    Implement API routes in server.js

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/dbConnect.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import caregiverRoutes from "./routes/caregiverRoutes.js";
+import familyRoutes from "./routes/familyRoutes.js";
+import availabilityRoutes from "./routes/availabilityRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 
-const app = express();
+
 dotenv.config();
 
+const app = express();
+
+// ConnectDatabase
+connectDB();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.send('Server is ready');
+app.use("/uploads", express.static("uploads"));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/client", clientRoutes);
+app.use("/api/caregivers", caregiverRoutes);
+app.use("/api/family", familyRoutes);
+app.use("/api/availability", availabilityRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.get("/api", (req, res) => {
+  res.send("Server is ready");
 });
 
+//Start the server
 const port = process.env.PORT || 3000;
 
-const MONGOURI = process.env.MONGO_URI;
-
-mongoose.connect(MONGOURI).then(()=>{
-    console.log("Databse id connected sccessfully")
-}) .catch((error) => console.log(error))
-
 app.listen(port, () => {
-    console.log('Serve at http://localhost:${port}');
+    console.log(`Server running at http://localhost:${port}`);
 });
