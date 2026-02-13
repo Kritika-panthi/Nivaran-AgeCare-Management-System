6c521c50122e87979ced285878d6b872a8c16656
Kritee <kritikapanthi45@gmail.com>
2026-02-13 10:00:00 +0545

    Create FamilyProfile data model

---
+++ models/familyProfile.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const familyProfileSchema = new mongoose.Schema(
+  {
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    photo: {
+      type: String,
+    },
+
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    age: {
+      type: Number,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+
+    phone: {
+      type: String,
+    },
+
+    emergencyContact: {
+      type: String,
+    },
+
+    livingAddress: {
+      type: String,
+    },
+
+    bloodGroup: {
+      type: String,
+      enum: ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"],
+    },
+
+    allergies: {
+      type: String,
+    },
+
+    mobilityLevel: {
+      type: String,
+      enum: ["WALKS ALONE", "USES STICK", "WHEELCHAIR", "BEDRIDDEN", "NONE"],
+    },
+
+    chronicConditions: {
+      type: [String],
+      default: [],
+    },
+
+    currentMedicines: {
+      type: [String],
+      default: [],
+    },
+
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("FamilyProfile", familyProfileSchema);
