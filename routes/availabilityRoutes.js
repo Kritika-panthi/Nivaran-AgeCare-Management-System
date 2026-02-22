a60a135d217399edccacf100f30f1417ae0d3371
Kritee <kritikapanthi45@gmail.com>
2026-02-22 10:00:00 +0545

    Create availability routes for caregiver schedule endpoints

---
+++ routes/availabilityRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
+
+import {
+  getMyWeeklyAvailability,
+  upsertWeeklyAvailability,
+  removeTimeRange,
+  getAvailableSlotsForCaregiver,
+} from "../controllers/availabilityController.js";
+
+const router = express.Router();
+
+// Get my weekly availability
+router.get(
+  "/",
+  authMiddleware,
+  roleMiddleware("caregiver"),
+  getMyWeeklyAvailability
+);
+
+// Add time range to a day
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("caregiver"),
+  upsertWeeklyAvailability
+);
+
+// Remove specific time range
+router.delete(
+  "/",
+  authMiddleware,
+  roleMiddleware("caregiver"),
+  removeTimeRange
+);
+
+
+
+// Get available hourly slots
+router.get("/slots", getAvailableSlotsForCaregiver);
+
+export default router;
