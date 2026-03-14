bde82eea1a6dd97b58d339fd496c10d6ebef0537
Kritee <kritikapanthi45@gmail.com>
2026-03-14 10:00:00 +0545

    Create notification routes for handling notification endpoints

---
+++ routes/notificationRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import {
+  getMyNotifications,
+  getUnreadNotificationCount,
+  markNotificationAsRead,
+  markAllNotificationsAsRead,
+} from "../controllers/notificationController.js";
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get("/unread-count", getUnreadNotificationCount);
+router.get("/", getMyNotifications);
+router.put("/read-all", markAllNotificationsAsRead);
+router.put("/:id/read", markNotificationAsRead);
+
+export default router;
